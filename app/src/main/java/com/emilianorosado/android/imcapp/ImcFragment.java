9ce5bfb33f0f34184333d60877929a9c92a2e96d
Emiliano Rosado <14470229@itcampeche.edu.mx>
2017-10-26 11:29:21 -0500

    Version inical del proyecto IMCApp. No incluye manejo de eventos

---
+++ app/src/main/java/com/emilianorosado/android/imcapp/ImcFragment.java
@@ -0,0 +1,29 @@
+package com.emilianorosado.android.imcapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Danny on 26/10/2017.
+ */
+
+public class ImcFragment  extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragmento_imc, container, false);
+
+        return v;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
